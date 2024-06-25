d91de354dc450c8a8b7e3582a6eb3ff66433a60b
Saransh Bhardwaj <saranshbhardwaj1999@gmail.com>
2024-06-26 00:07:31 +0530

    project initialization

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.indigo,
+        secondary: colors.yellow,
+        neutral: colors.gray,
+      },
+    },
+  },
+  plugins: [],
+};
+
